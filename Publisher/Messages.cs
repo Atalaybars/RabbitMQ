79920f833ff9dc3910d6e22594b9fb52a40e28fb
akif <akif414181@gmail.com>
2021-05-22 17:18:50 +0300

    Add Messages class to keep the messages in a separate file and this makes it cleaner and easier to maintain. If you know, you know :)

---
+++ Publisher/Messages.cs
@@ -0,0 +1,27 @@
+namespace Publisher
+{
+    public class Messages
+    {
+        public static string[] GetMessageArray()
+        {
+            var messages = new[]
+            {
+                "YO! YO! -- 1", 
+                "HEY GUYS WHATS UP? XD -- 2", 
+                "QUICK! I'M IN RUSH -- 3", 
+                "IS THIS THE FASTEST QUEU?!? :D -- 4",
+                "OH COME ON! -- 5",
+                "LOOKS GOOD -- 6",
+                "ARE YOU TIRED? -- 7",
+                "THAT SHOULD BE FUN -- 8",
+                "MESSAGE :) -- 9",
+                "WELL YOU MAY ASK -- 10",
+                "AM I HIGH OR SOMETHING -- 11",
+                "NOPE. JUST RELAX. -- 12",
+                "AND ENJOY THIS IMPRESSIVE TECH -- 13"
+            };
+
+            return messages;
+        }
+    }
+}
