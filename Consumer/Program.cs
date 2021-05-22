f1d5627e9927e2b7a13294bfb8b496b9dba7f517
akif <akif414181@gmail.com>
2021-05-22 17:19:32 +0300

    Add Consumer, ConsumerTwo, and Publisher files

---
+++ Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "taskQueu",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                
+                channel.BasicQos(0,1,false);            
+    
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    // make things slower to inspect the process
+                    Thread.Sleep(TimeSpan.FromSeconds(3));
+                    
+                    Console.WriteLine($"Received: {message}");
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+                
+                channel.BasicConsume(queue: "taskQueu",
+                    autoAck: false,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "taskQueu",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var messages = Messages.GetMessageArray();
+
+                    for (int i = 0; i <= 12; i++)
+                {
+                    string message = messages[i];
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properities = channel.CreateBasicProperties();
+                    properities.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "taskQueu",
+                        basicProperties: properities,
+                        body: body);
+                    Console.WriteLine($"Sent: {message}");    
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();  
+        }
+    }
+}
